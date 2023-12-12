6cd4b18501aee5c47df0e734386b39cf6a91ad0b
robinmin <luonbin@hotmail.com>
2023-12-12 17:22:46 +0800

    first batch commit to enable log/slog

--- config/app_config.go
+++ config/app_config.go
@@ -1,19 +1,7 @@
 package config
 
-import (
-	"fmt"
-
-	"github.com/robinmin/gin-starter/pkg/utility"
-)
-
 const AppName = "dragondoat"
 const AppVersion = "0.0.1"
-
-var __cfgFile string // global config file name
-
-func Setup(cfgFile string) {
-	__cfgFile = cfgFile
-}
 
 type AppConfig struct {
 	System struct {
@@ -22,6 +10,7 @@ type AppConfig struct {
 		EnableCORS         bool   `yaml:"enable_cors,omitempty"`
 		EnableAuth         bool   `yaml:"enable_auth,omitempty"`
 		ExternalSvrAddress string `yaml:"external_svr_address,omitempty"`
+		TrustedProxies     string `yaml:"trusted_proxies,omitempty"`
 	} `yaml:"system"`
 
 	Database struct {
@@ -32,29 +21,4 @@ type AppConfig struct {
 		User     string `yaml:"dbuser,omitempty"`
 		Password string `yaml:"dbpassword,omitempty"`
 	} `yaml:"database"`
-}
-
-func NewAppConfig() *AppConfig {
-	cfg, err := utility.LoadConfig[AppConfig](__cfgFile)
-	if err != nil {
-		fmt.Println("Failed to load yaml config file from " + __cfgFile + "[" + err.Error() + "]")
-		return &AppConfig{}
-	}
-
-	return cfg
-}
-
-func (cfg *AppConfig) SaveConfig() error {
-	return utility.SaveConfig(cfg, __cfgFile)
-}
-
-func (cfg *AppConfig) GetConnectionString() (string, error) {
-	switch cfg.Database.Type {
-	case "mysql":
-		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", cfg.Database.User, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Database), nil
-	case "sqlite3":
-		return fmt.Sprintf("file:%s?cache=shared&mode=rwc", cfg.Database.Database), nil
-	default:
-		return "", fmt.Errorf("unsupported database type: %s", cfg.Database.Type)
-	}
 }
