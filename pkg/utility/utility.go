73b14acc8f5f8aef531139d87f862dfa82a90bd7
robinmin <luonbin@hotmail.com>
2023-12-12 17:23:18 +0800

    first batch commit to enable log/slog

--- pkg/utility/utility.go
+++ pkg/utility/utility.go
@@ -1,38 +1,32 @@
 package utility
 
-import (
-	"os"
+// // LoadConfig 从指定的YAML文件中加载配置信息
+// func LoadConfig[T any](yamlFile string) (*T, error) {
+// 	data, err := os.ReadFile(yamlFile)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	"gopkg.in/yaml.v3"
-)
+// 	var config T
+// 	err = yaml.Unmarshal(data, &config)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-// LoadConfig 从指定的YAML文件中加载配置信息
-func LoadConfig[T any](yamlFile string) (*T, error) {
-	data, err := os.ReadFile(yamlFile)
-	if err != nil {
-		return nil, err
-	}
+// 	return &config, nil
+// }
 
-	var config T
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
+// // SaveConfig 将配置信息保存到指定的YAML文件中
+// func SaveConfig[T any](cfg *T, yamlFile string) error {
+// 	data, err := yaml.Marshal(cfg)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return &config, nil
-}
+// 	err = os.WriteFile(yamlFile, data, 0644)
+// 	if err != nil {
+// 		return err
+// 	}
 
-// SaveConfig 将配置信息保存到指定的YAML文件中
-func SaveConfig[T any](cfg *T, yamlFile string) error {
-	data, err := yaml.Marshal(cfg)
-	if err != nil {
-		return err
-	}
-
-	err = os.WriteFile(yamlFile, data, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
+// 	return nil
+// }
