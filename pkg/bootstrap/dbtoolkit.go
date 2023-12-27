6e315891c02d8061e992513e931a3aff6dc82775
robinmin <luonbin@hotmail.com>
2023-12-27 00:08:11 -0800

    feat: enable uber/fx for DI

---
+++ pkg/bootstrap/dbtoolkit.go
@@ -0,0 +1,62 @@
+package bootstrap
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+
+	// _ "gorm.io/driver/sqlite" // // Sqlite driver based on GGO
+	_ "github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+	// "github.com/robinmin/gin-starter/config"
+	// "github.com/robinmin/dragonboat/config"
+)
+
+type DBParams struct {
+	Type     string
+	Host     string
+	Port     int
+	Database string
+	User     string
+	Password string
+}
+
+func NewDBParams(dbtype string, dbhost string, dbport int, dbdatabase string, dbuser string, dbpassword string) *DBParams {
+	return &DBParams{
+		Type:     dbtype,
+		Host:     dbhost,
+		Port:     dbport,
+		Database: dbdatabase,
+		User:     dbuser,
+		Password: dbpassword,
+	}
+}
+
+func (param *DBParams) GetConnectionString() (string, error) {
+	switch param.Type {
+	case "mysql":
+		return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", param.User, param.Password, param.Host, param.Port, param.Database), nil
+	case "sqlite3":
+		return fmt.Sprintf("file:%s?cache=shared&mode=rwc", param.Database), nil
+	default:
+		return "", fmt.Errorf("unsupported database type: %s", param.Type)
+	}
+}
+
+type DBToolKit struct {
+	*sqlx.DB
+}
+
+func NewDB(param *DBParams) (*DBToolKit, error) {
+	conn_str, err := param.GetConnectionString()
+	if err != nil {
+		return nil, fmt.Errorf("unsupported database type: %s", param.Type)
+	}
+
+	db, err1 := sqlx.Connect(param.Type, conn_str)
+	return &DBToolKit{DB: db}, err1
+}
+
+// func NewQuery[T any](db *DBToolKit) *T {
+// 	// return [T]{db: db}
+// 	return new(T)
+// }
